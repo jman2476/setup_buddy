a53a0c21f599afc28ec54364eadaf25e3010a57e
Squirblechem <152367545+jman2476@users.noreply.github.com>
2025-11-03 17:33:11 -0500

    table components are now focusable

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { useState, useEffect, useRef } from 'react'
 import './App.css'
 
-// TODO: make all tables into an array
+// TODO: Show div with details on table highlight
 
 function App() {
   const startRef = useRef({ x: 0, y: 0 })
   const mouseRef = useRef({ x: 0, y: 0 })
-  const tableRef = useRef(3)
+  const tableRef = useRef(0)
   const [tableList, setTableList] = useState([])
   
   const dragStart = (event) => {
@@ -16,6 +16,8 @@ function App() {
     }
   }
 
+  // currently depricated
+  // see Table()
   function TableRect({number}) {
     const [offset, setOffSet] = useState({ x: 0, y: 0 })
     const [tableSize, setTableSize] = useState(60)
@@ -69,6 +71,15 @@ function App() {
     const [offset, setOffSet] = useState({ x: 0, y: 0 })
     const [tableSize, setTableSize] = useState(60)
 
+    function DataBox({}){
+
+      return(
+        <>
+        <p>{tableSize}</p>
+        </>
+      )
+    }
+
     useEffect(() => {
       const handleMouse = (event) => {
         mouseRef.current = {
@@ -92,6 +103,10 @@ function App() {
         y: prev.y + mouseRef.current.y - startRef.current.y
       }))
     }
+    const showData = (event) => {
+      console.log('Dragging table', number)
+    }
+
     return (
       <>
         {/* Prototype Round Table */}
@@ -112,9 +127,11 @@ function App() {
               >1</div> */}
         <div
           draggable={true}
+          tabIndex={0}
           className={`table-obj ${shape}`}
           onDragStart={dragStart}
           onDragEnd={dragEnd}
+          onFocus={showData}
           style={{
             position: "absolute",
             top: `${offset.y + 100 * number}px`,
@@ -129,16 +146,6 @@ function App() {
     )
   }
 
-  // const dragEnd = (event) => {
-  //   mouseRef.current = {
-  //       x: event.clientX,
-  //       y: event.clientY
-  //     }
-  //   setOffSet(prev => ({
-  //     x: prev.x + mouseRef.current.x - startRef.current.x,
-  //     y: prev.y + mouseRef.current.y - startRef.current.y
-  //   }))
-  // }
   const tableMaker = (event) => {
     const tShape = tableRef.current%2 === 0? 'circle' : 'rectangle'
     const newTable = <Table number={tableRef.current} shape={tShape} key={tableRef.current} />
@@ -147,22 +154,10 @@ function App() {
     console.log(tableRef.current)
   }
 
-
-  const arrayOne = [
-    <Table number={3} key='1'/>,
-    <Table number={4} key='2'/>,
-    <Table number={5} key='3'/>,
-    <TableRect number={6} key='4'/>
-  ]
-
-  console.log(arrayOne, 'arrayOne'
-  )
-  
   return (
     <>
       <div id="toolbar">
         <h2 className='title'>Toolbar Time</h2>
-        {/* <input className="input" type="number" value={tableSize} onChange={e=>setTableSize(e.target.value)}/> */}
       </div>
       <div id="setup">
         <h2 className='title setup'>Your setup here:</h2>
@@ -170,10 +165,7 @@ function App() {
           <button
             onClick={tableMaker}
           >Make new table</button>
-          <Table number={1} />
-          <TableRect number={2} />
           {tableList}
-          {/* {arrayOne} */}
         </div>
       </div>
     </>
