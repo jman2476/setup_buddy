a42dfd5dc3f135bbeb02b22d7828b803e97e9dc0
Jeremy M <jman2476@live.com>
2025-11-06 14:26:00 -0500

    expanded file structure, adds Table class

---
+++ src/models/table.jsx
@@ -0,0 +1,24 @@
+export default class Table {
+    shape = 'circle'; //circle or rectangle
+    length = 6;
+    width = this.shape==='circle'? null:4;
+    
+    constructor(shape, length, width) {
+        try {
+            if (shape === 'circle') {
+                this.shape = shape
+                this.length = length
+            } else if (shape === 'rectangle') {
+                this.shape = shape
+                this.length = length
+                this.width = width
+            } else {
+                throw new Error('Table must be circle or rectangle')
+            }
+        } catch (err) {
+            console.log(err)
+            return err
+        }
+    }
+
+}
