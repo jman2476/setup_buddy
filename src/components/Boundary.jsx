1cbbd46c2bfdcef1fa9f0a8a7eadc9e760e66fd6
Jeremy M <jman2476@live.com>
2025-11-14 13:20:42 -0500

    basic setup for detecting click in Boundary component

--- src/components/Boundary.jsx
+++ src/components/Boundary.jsx
@@ -1,7 +1,42 @@
+import { useRef, useState } from "react"
+
 function Boundary ({ children }) {
+    const [pointList, setPointList] = useState()
+    const editBoundRef = useRef(false)
+
+    const handleEditButton = () => {
+        editBoundRef.current= !editBoundRef.current
+        console.log(editBoundRef.current)
+    }
+
+    // if editBoundRef===true, click field to create points
+    const handleSetBound = (event) => {
+        try {
+            console.log('handle set boundary')
+            if (editBoundRef.current){
+                console.log('select point for boundary')
+            } else {
+                throw new Error('Boundary edit is toggled off')
+            }
+        } catch(err) {
+            console.log(err)
+        }
+    }
+
     return (
         <>
-            <div>
+            <button
+                style={{
+                    position: 'absolute',
+                    top: '-10px',
+                    right: '-50px'
+                }}
+                onClick={handleEditButton}
+                >Edit boundaries</button>
+            <div
+                onClick={e=>handleSetBound(e)}
+                className="boundary"
+            > Potato
                 {children}
             </div>
         </>
