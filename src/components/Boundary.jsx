2b319ea09379ce3268b9d9714238a4a5415c8b35
Jeremy M <jman2476@live.com>
2025-11-14 14:36:40 -0500

    adds BoundaryPoint, and can create them by setting toggle and clicking boundary area

--- src/components/Boundary.jsx
+++ src/components/Boundary.jsx
@@ -1,8 +1,9 @@
 import { useRef, useState } from "react"
 
 function Boundary ({ children }) {
-    const [pointList, setPointList] = useState()
+    const [pointList, setPointList] = useState([])
     const editBoundRef = useRef(false)
+    const pointCounter = useRef(0)
 
     const handleEditButton = () => {
         editBoundRef.current= !editBoundRef.current
@@ -15,12 +16,39 @@ function Boundary ({ children }) {
             console.log('handle set boundary')
             if (editBoundRef.current){
                 console.log('select point for boundary')
+                const mousePosition = {
+                    x: event.clientX,
+                    y: event.clientY
+                }
+                console.log(mousePosition, 'click')
+                pointCounter.current++
+                setPointList(arr => [...arr, mousePosition])
             } else {
                 throw new Error('Boundary edit is toggled off')
             }
         } catch(err) {
             console.log(err)
         }
+    }
+    console.log('Point List', pointList)
+
+    // Can create points, but the offset is wrong
+    // needs to be adjusted to account for the 
+    // Boundary component position
+    function BoundaryPoint ({ positionObj }){
+        console.log(positionObj,'point position')
+        return (
+            <>
+                <div 
+                    style={{
+                        position:'absolute',
+                        top: `${positionObj.y}px`,
+                        left: `${positionObj.x}px`
+                    }}>
+                    {'\u2022'}
+                </div>
+            </>
+        )
     }
 
     return (
@@ -37,6 +65,14 @@ function Boundary ({ children }) {
                 onClick={e=>handleSetBound(e)}
                 className="boundary"
             > Potato
+                {pointList.map((obj,index)=>{
+                    console.log(obj,'point obj')
+                    return (
+                        <BoundaryPoint 
+                            positionObj={obj} 
+                            key={index+'point'}/>
+                    )
+                })}
                 {children}
             </div>
         </>
