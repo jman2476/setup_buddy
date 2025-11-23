da9e2f7cfce78b7e52c8cf88bbf3890468051616
Squirblechem <152367545+jman2476@users.noreply.github.com>
2025-11-23 17:54:08 -0500

    updated Table class to use TypeScript

--- src/models/table.tsx
+++ src/models/table.tsx
@@ -1,50 +1,58 @@
 class TableCore {
-
+   shape: string
+   angle: number
 }
 
 class TableCon extends TableCore {
-    static make(shape,angle,x,y) {
-        if (shape === 'circle') return new RoundTable(shape,angle,x)
-        if (shape === 'rectangle') return new LongTable(shape,angle,x,y)
-        if (shape === 'square') return new SquareTable(shape,angle,x)
-    }
+   static make(shape: string, angle: number, x: number, y: number) {
+      if (shape === 'circle') return new RoundTable(shape, angle, x)
+      if (shape === 'rectangle') return new LongTable(shape, angle, x, y)
+      if (shape === 'square') return new SquareTable(shape, angle, x)
+   }
 }
 
 class RoundTable extends TableCore {
-    constructor(shape, angle, diameter) {
-        super()
-        this.shape = shape ?? 'circle'
-        this.angle = angle | 0
-        this.diameter = diameter ?? 60 
-        this.diameter = Number(this.diameter)
-    }
+   diameter: number
+
+   constructor(shape: string, angle: number, diameter: number) {
+      super()
+      this.shape = shape ?? 'circle'
+      this.angle = angle | 0
+      this.diameter = diameter ?? 60
+      // this.diameter = Number(this.diameter)
+   }
 }
 
 class LongTable extends TableCore {
-    constructor(shape, angle, length, width) {
-        super()
-        this.shape = shape ?? 'rectangle'
-        this.angle = angle | 0
-        this.length = length ?? 80
-        this.width = width ?? 20
-        this.length = Number(this.length)
-        this.width = Number(this.width)
-    }
+   length: number
+   width: number
+
+   constructor(shape: string, angle: number, length: number, width: number) {
+      super()
+      this.shape = shape ?? 'rectangle'
+      this.angle = angle | 0
+      this.length = length ?? 80
+      this.width = width ?? 20
+      // this.length = Number(this.length)
+      // this.width = Number(this.width)
+   }
 }
 
 class SquareTable extends TableCore {
-    constructor(shape, angle, side) {
-        super()
-        this.shape = shape ?? 'square'
-        this.angle = angle | 0
-        this.side = side ?? 40
-        this.side = Number(this.side)
-    }
+   side: number
+
+   constructor(shape: string, angle: number, side: number) {
+      super()
+      this.shape = shape ?? 'square'
+      this.angle = angle | 0
+      this.side = side ?? 40
+      // this.side = Number(this.side)
+   }
 }
 
 export {
-    TableCon,
-    RoundTable,
-    LongTable,
-    SquareTable
+   TableCon,
+   RoundTable,
+   LongTable,
+   SquareTable
 }
