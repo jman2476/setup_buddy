e7d376ca399dd0f865e43ebb00560f926fb7ccc8
Squirblechem <152367545+jman2476@users.noreply.github.com>
2025-11-25 00:26:54 -0500

    renamed /src/compononents/index and Databox to .tsx, starting migration

--- src/components/Databox.tsx
+++ src/components/Databox.tsx
@@ -1,5 +1,4 @@
 import { useState, useEffect, useRef } from 'react'
-import { RoundTable, LongTable, SquareTable } from '../models/table.jsx'
 
 function DataBox({ field, value }) {
    // // opt-out of auto-memoization
