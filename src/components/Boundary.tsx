c6da61a83638be6962d9bd216912aa881feb5517
Squirblechem <152367545+jman2476@users.noreply.github.com>
2025-11-25 02:08:30 -0500

    starting App.tsx migration

--- src/components/Boundary.tsx
+++ src/components/Boundary.tsx
@@ -91,10 +91,10 @@ function Boundary({ children, rotatedPoints }: BoundaryProps) {
          // Triangle
          // const testVertices = [{ x: 400, y: 100 }, { x: 400, y: 500 }, { x: 400+200*Math.sqrt(3), y: 300 }]
          // Pentagon
-         const c1 = Math.cos(Math.PI * 2 / 5) * 200
-         const c2 = Math.cos(Math.PI / 5) * 200
-         const s1 = Math.sin(Math.PI * 2 / 5) * 200
-         const s2 = Math.sin(Math.PI * 4 / 5) * 200
+         // const c1 = Math.cos(Math.PI * 2 / 5) * 200
+         // const c2 = Math.cos(Math.PI / 5) * 200
+         // const s1 = Math.sin(Math.PI * 2 / 5) * 200
+         // const s2 = Math.sin(Math.PI * 4 / 5) * 200
          // const testVertices = [{x: 300,y: 500},{x: s1+300,y: c1+300 },{x: s2+300,y: -1*c2+300},{x: -1*s2+300,y: -1*c2+300},{x: -1*s1+300,y: c1+300}]
          setPointList(testVertices)
       } catch (error) {
